73b60003c2055e18f29762af0a864e680d681fe9
OutrageousPotato <jonathan@gibsonnotes.com>
2017-09-03 17:52:20 -0700

    Finished challenge

---
+++ Postal Calculator/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Postal_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:&nbsp;
+        <asp:TextBox ID="WidthTextBox" runat="server" AutoPostBack="True" OnTextChanged="WidthTextBox_TextChanged">1</asp:TextBox>
+        <br />
+        Height:&nbsp;
+        <asp:TextBox ID="HeightTextBox" runat="server" AutoPostBack="True" OnTextChanged="HeightTextBox_TextChanged">1</asp:TextBox>
+        <br />
+        Length:&nbsp;
+        <asp:TextBox ID="LengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="LengthTextBox_TextChanged">1</asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="GroundButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="GroundButton_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="AirButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="AirButton_CheckedChanged" Text="Air" />
+        <br />
+        <asp:RadioButton ID="NextDayButton" runat="server" AutoPostBack="True" GroupName="ShippingMethod" OnCheckedChanged="NextDayButton_CheckedChanged" Text="Next Day" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Postal Calculator/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Postal_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        double shippingVolume = 0.0;
+        double methodMultiplier = 0.0;
+
+        public double getVolume()
+        {
+            shippingVolume = double.Parse(WidthTextBox.Text) *
+                double.Parse(LengthTextBox.Text) *
+                double.Parse(HeightTextBox.Text);
+            return shippingVolume;
+        }
+
+        public void checkMethod()
+        {
+            if (GroundButton.Checked)
+            {
+                methodMultiplier = .15;
+            }
+            else if (AirButton.Checked)
+            {
+                methodMultiplier = .25;
+            }
+            else
+            {
+                methodMultiplier = .45;
+            }
+        }
+
+        public double getCost(double shippingVolume, double methodMultiplier)
+        {
+            double totalCost = shippingVolume * methodMultiplier;
+            return totalCost;
+        }
+
+        public void showResult()
+        {
+            checkMethod();
+            double answer = getCost(getVolume(), methodMultiplier);
+            resultLabel.Text = String.Format("It will cost {0:C} to ship your package", answer);
+        }
+
+
+        protected void WidthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+
+        protected void HeightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+
+        protected void LengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+
+        protected void GroundButton_CheckedChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+
+        protected void AirButton_CheckedChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+
+        protected void NextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            showResult();
+        }
+    }
+}
